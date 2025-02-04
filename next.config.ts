343be7cc6027d63e8c3d83fd447d1b5367214558
Łukasz Szewczyk <szewczyk134@gmail.com>
2025-02-04 13:04:24 +0100

    Revalidate path

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,20 @@ import type { NextConfig } from "next";
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'static.colourlovers.com',
+      },
+    ],
+  },
   /* config options here */
 };
 
 async function setupDevCloudflarePlatform() {
   await setupDevPlatform();
-}  
+}
 
 setupDevCloudflarePlatform()
 
---
+++ src/app/revalidate/page.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+
+export interface Pattern {
+  id: number
+  title: string
+  userName: string
+  numViews: number
+  numVotes: number
+  numComments: number
+  numHearts: number
+  rank: number
+  dateCreated: string
+  hex: string
+  rgb: Rgb
+  hsv: Hsv
+  description: string
+  url: string
+  imageUrl: string
+  badgeUrl: string
+  apiUrl: string
+}
+
+export interface Rgb {
+  red: number
+  green: number
+  blue: number
+}
+
+export interface Hsv {
+  hue: number
+  saturation: number
+  value: number
+}
+
+// Next.js will invalidate the cache when a
+// request comes in, at most once every 10 seconds.
+export const revalidate = 10
+
+
+export default async function Page() {
+  const pattern: Pattern[] = await fetch(`https://www.colourlovers.com/api/patterns/random?format=json`).then(
+    (res) => res.json()
+  )
+  return (
+    <main>
+      <p>{pattern[0].id}</p>
+      <p>{pattern[0].title}</p>
+      <p>{pattern[0].imageUrl}</p>
+      <Image alt={pattern[0].title} src={pattern[0].imageUrl} width={1000} height={1000}/>
+    </main>
+  )
+}
